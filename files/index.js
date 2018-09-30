3c9e21e1232a9b886e04dec13f60a26295e1794f
Van Hai Le <vhail17.9.98@gmail.com>
2018-09-30 11:52:33 +0000

    Limit files' size

--- files/index.js
+++ files/index.js
@@ -6,8 +6,7 @@ const {spawn} = require('child_process');
 http.createServer((req, res) => {
 	if (req.url === "/printfile") {
 		var form = new formidable.IncomingForm();
-		if (form.maxFileSize < 2500) {
-			form.parse(req, function(err, fields, files) {
+		form.parse(req, function(err, fields, files) {
 			console.log(files.filetoprint);
 			var oldPath = files.filetoprint.path;
 			var newPath = '/home/mint/Downloads/printFile/files/' + files.filetoprint.name;
@@ -15,23 +14,20 @@ http.createServer((req, res) => {
 				if (err) throw err;
 				return res.end('Uploaded and moved to files folder');
 			});
-			
-			(function(test, callback) {
+			/*(function(test, callback) {
 				spawn('lp', ['-o', 'media=A4', newPath]);
 				callback();	
 			})(1, function() {
-		 		console.log('Printing');
-			});
+				console.log('Printing');
+			});*/
 			
-			});
-		} else {
-			console.log('Oversize');
-		}
+		});
 	}
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('<form action="printfile" method="post" enctype="multipart/form-data">');
 	res.write('<input type="file" name="filetoprint"><br/>');
-	res.write('<button id="submitbtn" type="submit">Print</button>');
+	res.write('<button type="submit">Print</button>');
 	return res.end();
 }).listen(8080, () => {
-	console.log('Server is listening on port 8080'); });
+	console.log('Server is listening on port 8080');
+});
